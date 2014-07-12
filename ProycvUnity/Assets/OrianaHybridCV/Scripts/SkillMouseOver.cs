a3ba488675d36987c5f115b8d7c5ec5e2eaffd8a
Piffia <kamelka__@gmail.com>
2014-07-12 18:14:15 +0200

    FX Mose over

---
+++ ProycvUnity/Assets/OrianaHybridCV/Scripts/SkillMouseOver.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillMouseOver : MonoBehaviour {
+
+	public SpriteRenderer ButtonFX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseOver()
+	{ 
+		if(ButtonFX)
+		{
+			ButtonFX.enabled = true;
+		}
+	}
+
+	void OnMouseExit()
+	{ 
+		if(ButtonFX)
+		{
+			ButtonFX.enabled = false;
+		}
+	}
+
+
+}
