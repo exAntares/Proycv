e702e6c8498a745f658501dfe2e5d88269f1a52a
exAntares <luis_andres81@hotmail.com>
2014-11-02 11:15:28 +0100

    New SkillInfo, and skill tree

--- ProycvUnity/Assets/OrianaHybridCV/Scripts/Skills/SkillMouseOver.cs
+++ ProycvUnity/Assets/OrianaHybridCV/Scripts/Skills/SkillMouseOver.cs
@@ -2,23 +2,45 @@
 using System.Collections;
 
 public class SkillMouseOver : MonoBehaviour {
-
+    
     public string SkillRank = "Rank 1/10";
-
     [Multiline]
     public string SkillDescription = "Soy una Skill";
-
     public TextMesh TextRenderer;
+
+    private string SkillTitle;
+    private GameObject SkillText;
 
 	// Use this for initialization
 	void Start () {
+
+        SkillText = GameObject.FindGameObjectWithTag("SkillText");
+        TextRenderer = SkillText.GetComponentInChildren<TextMesh>();
+
+        SkillTitle = "<color=red><b>" + gameObject.name + "</b></color>\n";
+        SkillRank = "<size=15><color=yellow>" + SkillRank + "</color>\n";
+        SkillDescription = SkillDescription + "</size>";
+
+        UpdateText();
+	}
+
+    void OnMouseOver()
+    {
+        SkillText.SetActive(true);
+        UpdateText();
+    }
+
+    void OnMouseExit()
+    {
+        SkillText.SetActive(false);
+    }
+
+    void UpdateText()
+    {
         if (TextRenderer)
         {
-            string SkillTitle = "<color=red><b>" + gameObject.name + "</b></color>\n";
-            SkillRank = "<size=15><color=yellow>" + SkillRank + "</color>\n";
-            SkillDescription = SkillDescription + "</size>";
-
             TextRenderer.text = SkillTitle + SkillRank + SkillDescription;
         }
-	}
+    }
+
 }
