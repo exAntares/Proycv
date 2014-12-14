dcf4425782eb75753fc2e3c355110d75cf26aa8b
exAntares <luis_andres81@hotmail.com>
2014-12-14 17:28:06 +0100

    Link Reward

---
+++ ProycvUnity/Assets/OrianaHybridCV/Scripts/utils/LinkScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkScript : MonoBehaviour
+{
+    public string link = "";
+
+    [HideInInspector]
+    public Texture2D cursorTextureNormal;
+    [HideInInspector]
+    public Texture2D cursorTextureLink;
+    [HideInInspector]
+    public CursorMode cursorMode = CursorMode.Auto;
+    [HideInInspector]
+    public Vector2 hotSpot = Vector2.zero;
+
+    void OnMouseOver()
+    {
+        Cursor.SetCursor(cursorTextureLink, Vector2.zero, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+
+    void OnMouseUpAsButton()
+    {
+        Handlelink();
+    }
+
+    void Handlelink()
+    {
+        if (link != "")
+        {
+            Application.ExternalEval("window.open('" + link + "','_blank')");
+        }
+    }
+
+}
